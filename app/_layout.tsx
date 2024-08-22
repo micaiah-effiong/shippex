8045863f1a0fea9c1a4197adbfc97032ac6902ff
Micah Effiong <micaiah.effiong@gmail.com>
2024-08-22 19:31:43 +0100

    add SF-Pro-Text fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,15 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -13,7 +17,26 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+
+    "SF-Pro-Text-Black": require("../assets/fonts/SFProText/SF-Pro-Text-Black.otf"),
+    "SF-Pro-Text-BlackItalic": require("../assets/fonts/SFProText/SF-Pro-Text-BlackItalic.otf"),
+    "SF-Pro-Text-Bold": require("../assets/fonts/SFProText/SF-Pro-Text-Bold.otf"),
+    "SF-Pro-Text-BoldItalic": require("../assets/fonts/SFProText/SF-Pro-Text-BoldItalic.otf"),
+    "SF-Pro-Text-Heavy": require("../assets/fonts/SFProText/SF-Pro-Text-Heavy.otf"),
+    "SF-Pro-Text-HeavyItalic": require("../assets/fonts/SFProText/SF-Pro-Text-HeavyItalic.otf"),
+    "SF-Pro-Text-Light": require("../assets/fonts/SFProText/SF-Pro-Text-Light.otf"),
+    "SF-Pro-Text-LightItalic": require("../assets/fonts/SFProText/SF-Pro-Text-LightItalic.otf"),
+    "SF-Pro-Text-Medium": require("../assets/fonts/SFProText/SF-Pro-Text-Medium.otf"),
+    "SF-Pro-Text-MediumItalic": require("../assets/fonts/SFProText/SF-Pro-Text-MediumItalic.otf"),
+    "SF-Pro-Text-Regular": require("../assets/fonts/SFProText/SF-Pro-Text-Regular.otf"),
+    "SF-Pro-Text-RegularItalic": require("../assets/fonts/SFProText/SF-Pro-Text-RegularItalic.otf"),
+    "SF-Pro-Text-Semibold": require("../assets/fonts/SFProText/SF-Pro-Text-Semibold.otf"),
+    "SF-Pro-Text-SemiboldItalic": require("../assets/fonts/SFProText/SF-Pro-Text-SemiboldItalic.otf"),
+    "SF-Pro-Text-Thin": require("../assets/fonts/SFProText/SF-Pro-Text-Thin.otf"),
+    "SF-Pro-Text-ThinItalic": require("../assets/fonts/SFProText/SF-Pro-Text-ThinItalic.otf"),
+    "SF-Pro-Text-Ultralight": require("../assets/fonts/SFProText/SF-Pro-Text-Ultralight.otf"),
+    "SF-Pro-Text-UltralightItalic": require("../assets/fonts/SFProText/SF-Pro-Text-UltralightItalic.otf"),
   });
 
   useEffect(() => {
@@ -27,8 +50,8 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack initialRouteName='index'>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 // tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "app-blue": "#2F50C1",
+      },
+      fontFamily: {
+        SFProTextBlack: ["SF-Pro-Text-Black"],
+        SFProTextBlackItalic: ["SF-Pro-Text-BlackItalic"],
+        SFProTextBold: ["SF-Pro-Text-Bold"],
+        SFProTextBoldItalic: ["SF-Pro-Text-BoldItalic"],
+        SFProTextHeavy: ["SF-Pro-Text-Heavy"],
+        SFProTextHeavyItalic: ["SF-Pro-Text-HeavyItalic"],
+        SFProTextLight: ["SF-Pro-Text-Light"],
+        SFProTextLightItalic: ["SF-Pro-Text-LightItalic"],
+        SFProTextMedium: ["SF-Pro-Text-Medium"],
+        SFProTextMediumItalic: ["SF-Pro-Text-MediumItalic"],
+        SFProTextRegular: ["SF-Pro-Text-Regular"],
+        SFProTextRegularItalic: ["SF-Pro-Text-RegularItalic"],
+        SFProTextSemibold: ["SF-Pro-Text-Semibold"],
+        SFProTextSemiboldItalic: ["SF-Pro-Text-SemiboldItalic"],
+        SFProTextThin: ["SF-Pro-Text-Thin"],
+        SFProTextThinItalic: ["SF-Pro-Text-ThinItalic"],
+        SFProTextUltralight: ["SF-Pro-Text-Ultralight"],
+        SFProTextUltralightItalic: ["SF-Pro-Text-UltralightItalic"],
+      },
+    },
   },
   plugins: [],
-}
+};
